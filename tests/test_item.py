089e710111ac7448dbf77851f08fcc6e456c9e07
iafominykh <iafominykh@yandex.ru>
2023-04-20 20:18:03 +0300

    hw1 whis tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def return_date():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(return_date):
+    assert return_date.calculate_total_price() == 200000
+
+
+def test_apply_discount(return_date):
+    return_date.pay_rate = 0.7
+    return_date.apply_discount()
+    assert return_date.price == 7000
