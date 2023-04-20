5c814b4c520f7b75d26372f58c3f1c5801c3d46b
iafominykh <iafominykh@yandex.ru>
2023-04-20 21:06:54 +0300

    hw1 whis tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -6,14 +6,14 @@ from src.item import Item
 
 @pytest.fixture
 def return_date():
-    return Item("Смартфон", 10000, 20)
+    return Item("Смартфон", 5000, 20)
 
 
 def test_calculate_total_price(return_date):
-    assert return_date.calculate_total_price() == 200000
+    assert return_date.calculate_total_price() == 100000
 
 
 def test_apply_discount(return_date):
     return_date.pay_rate = 0.7
     return_date.apply_discount()
-    assert return_date.price == 7000
+    assert return_date.price == 3500
