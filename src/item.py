b11352c8a02e4d201ec768106c1e6a7266fc375b
iafominykh <iafominykh@yandex.ru>
2023-04-24 17:56:06 +0300

    hw2 whis bad tests

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
+        # Item.all.append(self)
+        self.__class__.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -37,3 +41,27 @@ class Item:
         module_name = "__main__"
         class_name = self.__class__.__name__
         return f"<{module_name}.{class_name} object at {hex(id(all))}>"
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            raise Exception('Длина наименования товара превышает 10 символов')
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('../src/items.csv') as file:
+            reader = csv.DictReader(file)
+            next(reader)
+            for row in reader:
+                cls(row['name'], float(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(number):
+        return_number = float(number)
+        return int(return_number)
--- tests/test_item.py
+++ tests/test_item.py
@@ -17,3 +17,19 @@ def test_apply_discount(return_date):
     return_date.pay_rate = 0.7
     return_date.apply_discount()
     assert return_date.price == 3500
+
+
+def test_string_to_number():
+    assert Item.string_to_number("7.77") == 7
+
+def test_name():
+    item1 = Item('Microwave', 10000, 1)
+    item2 = Item('Refregerator', 5000, 1)
+    assert len(item1.name) <= 10
+    assert item2.name == 'Refregerator'
+
+def test_instantiate_from_csv():
+    item1 = Item('Смартфон', 10000, 1)
+    item1.instantiate_from_csv()
+    assert len(Item.all) == 5
+    assert Item.all[0].name == 'Смартфон'
